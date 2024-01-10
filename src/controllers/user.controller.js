2f351c8a7ae430ecb19e5144be5a11338e1ef422
padariya-tech <nijpadariya@gmail.com>
2024-01-10 22:29:05 +0530

    checking the working using postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
     // return response
 
     const{fullName , email , username , password }=req.body
-    console.log("email ",email);
+    // console.log("email ",email);
 
     if(
         [fullName,email,username,password].some((field)=>field?.trim()==="")
@@ -27,14 +27,19 @@ const registerUser = asyncHandler(async (req, res) => {
        throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser =  User.findOne({$or:[{email},{username}]})
+    const existedUser = await User.findOne({$or:[{email},{username}]})
 
     if(existedUser){
         throw new ApiError(409,"User with email or username already exist")
     }
 
     const avatarLocalPath= req.files?.avatar[0]?.path;
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath)
     {
